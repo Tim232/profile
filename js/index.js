ed97a6da7f2f0db0d3b3b9702c6f0a17384331e3
ditto7890 <kwak071219@gmail.com>
2020-07-28 23:32:06 +0900

    버그 수정

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 var canvas = document.getElementById('canvas')
 var ctx = canvas.getContext('2d')
+var isIE = false || !!document.documentMode
 
 if (!Element.prototype.remove) {
     Element.prototype.remove = function () {
